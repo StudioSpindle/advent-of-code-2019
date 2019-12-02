d0f153f6667bd6314de4da2ff3fc371ffc45851f
Vledder <remi@studiospindle.nl>
2019-12-02 09:55:28 +0100

    dry code, make code more descriptive

--- day1/script.js
+++ day1/script.js
@@ -2,18 +2,20 @@ const { test, expect } = require('../assertions');
 const { data } = require('./data');
 
 /**
- * fuelRequired
+ * requiredModuleFuel
+ * @description take a module's mass, divide by three, round down, and subtract 2
  * @param mass
  * @returns {number} fuel required to launch a module
  */
-const fuelRequired = (mass) => Math.floor(mass / 3) - 2;
+const requiredModuleFuel = (mass) => Math.floor(mass / 3) - 2;
 
 /**
  * fuelRequiredAll
+ * @description calculate fuel for multiple modules
  * @param values
  * @returns {number} sum of fuel required for all modules
  */
-const fuelRequiredAll = (values) => values.reduce((total, value) => total + fuelRequired(value), 0);
+const requiredModuleFuelAll = (values) => values.reduce((total, value) => total + requiredModuleFuel(value), 0);
 
 /**
  * Tests
@@ -28,19 +30,19 @@ const testValues = [
 
 testValues.forEach(function(entry) {
   test(`for the mass of ${entry[0]} fuelRequired calculates ${entry[1]} fuel`, () => {
-    expect(fuelRequired(entry[0])).toBe(entry[1]);
+    expect(requiredModuleFuel(entry[0])).toBe(entry[1]);
   });
 });
 
-const values = [12, 14, 1969, 100756];
+const testValuesInput = testValues.map((value) => value[0]);
 const testValuesTotal = testValues.reduce((total, value) => total + value[1], 0);
 
 test(`fuelRequiredAll`, () => {
-  expect(fuelRequiredAll(values)).toBe(testValuesTotal);
+  expect(requiredModuleFuelAll(testValuesInput)).toBe(testValuesTotal);
 });
 
 /**
  * Answer
  */
 
-console.log(`Answer: ${fuelRequiredAll(data)}`);
+console.log(`Answer: ${requiredModuleFuelAll(data)}`);
