2a4b579b449dbce9836a49b8188d87c8c78aa1f6
Vledder <remi@studiospindle.nl>
2019-12-02 19:03:33 +0100

    day 1, part 2
    source of recursive function pattern: https://javascript.info/recursion

--- day1/script.js
+++ day1/script.js
@@ -2,12 +2,31 @@ const { test, expect } = require('../assertions');
 const { data } = require('./data');
 
 /**
+ * calcModuleFuel
+ * @description take a module's mass, divide by three, round down, and subtract 2.
+ * @param number
+ * @returns {number}
+ */
+const calcModuleFuel = (number) => Math.floor(number / 3) - 2;
+
+/**
  * requiredModuleFuel
- * @description take a module's mass, divide by three, round down, and subtract 2
+ * @description recursively calculates the fuel that is necessary because of the extra weight of the fuel itself.
  * @param mass
  * @returns {number} fuel required to launch a module
  */
-const requiredModuleFuel = (mass) => Math.floor(mass / 3) - 2;
+const requiredModuleFuel = (mass) => {
+  // shorthand example:
+  // return (calcModuleFuel(mass) <= 0) ? 0 : calcModuleFuel(mass) + requiredModuleFuel(calcModuleFuel(mass));
+
+  // more readable example
+  const fuelRemaining = calcModuleFuel(mass) >= 0;
+  if (!fuelRemaining) {
+    return 0;
+  }
+  const fuel = calcModuleFuel(mass);
+  return fuel + requiredModuleFuel(fuel);
+};
 
 /**
  * fuelRequiredAll
@@ -21,24 +40,20 @@ const requiredModuleFuelAll = (values) => values.reduce((total, value) => total 
  * Tests
  */
 
-const testValues = [
-  [12, 2],
-  [14, 2],
-  [1969, 654],
-  [100756, 33583]
-];
-
-testValues.forEach(function(entry) {
-  test(`for the mass of ${entry[0]} fuelRequired calculates ${entry[1]} fuel`, () => {
-    expect(requiredModuleFuel(entry[0])).toBe(entry[1]);
-  });
+test('module of mass 14 requires 2 fuel', () => {
+  expect(requiredModuleFuel(14)).toBe(2);
 });
 
-const testValuesInput = testValues.map((value) => value[0]);
-const testValuesTotal = testValues.reduce((total, value) => total + value[1], 0);
+test('module of mass 1969 requires 966 fuel', () => {
+  expect(requiredModuleFuel(1969)).toBe(966);
+});
+
+test('module of mass 100756 requires 50346 fuel', () => {
+  expect(requiredModuleFuel(100756)).toBe(50346);
+});
 
 test(`fuelRequiredAll`, () => {
-  expect(requiredModuleFuelAll(testValuesInput)).toBe(testValuesTotal);
+  expect(requiredModuleFuelAll([14, 1969, 100756])).toBe(51314);
 });
 
 /**
