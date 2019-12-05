fe835a99b691def149eef6917265e33e53e98785
Vledder <remi@studiospindle.nl>
2019-12-05 22:00:16 +0100

    day 3, part 1

--- day3/script.js
+++ day3/script.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('../assertions');
+const { locations } = require('./locations');
 
 const regExpLetter = /[a-zA-Z]/;
 const regExtDigit = /\d+/;
@@ -97,21 +98,23 @@ const intersectionCalc = (wirePaths1, wirePaths2) => {
   // console.table(commonPaths);
 
   const filteredCommonPaths = commonPaths.filter(notStartingLocation);
+  const pointsFromZero = (number) => Math.abs(number - 0);
+  // TODO: make this part more readable
   const lowest = filteredCommonPaths.reduce((prev, curr) => {
-    return prev.locationOnGrid.x + prev.locationOnGrid.y < curr.locationOnGrid.x + curr.locationOnGrid.y ? prev : curr
+    return pointsFromZero(prev.locationOnGrid.x) + pointsFromZero(prev.locationOnGrid.y) < pointsFromZero(curr.locationOnGrid.x) + pointsFromZero(curr.locationOnGrid.y) ? prev : curr;
   });
 
   // DEBUG
-  console.table(lowest);
+  // console.table(lowest);
 
   return lowest.locationOnGrid.x + lowest.locationOnGrid.y;
 };
 
-// test('test [["R8","U5","L5","D3"],["U7","R6","D4","L4"]]', () => {
-//   const testProgram = [["R8","U5","L5","D3"],["U7","R6","D4","L4"]];
-//   const testProgramResult = 6;
-//   expect(intersectionCalc(testProgram[0], testProgram[1])).toEqual(testProgramResult);
-// });
+test('test [["R8","U5","L5","D3"],["U7","R6","D4","L4"]]', () => {
+  const testProgram = [["R8","U5","L5","D3"],["U7","R6","D4","L4"]];
+  const testProgramResult = 6;
+  expect(intersectionCalc(testProgram[0], testProgram[1])).toEqual(testProgramResult);
+});
 
 test('test [["R75","D30","R83","U83","L12","D49","R71","U7","L72"],["U62","R66","U55","R34","D71","R55","D58","R83"]]', () => {
   const testProgram = [["R75","D30","R83","U83","L12","D49","R71","U7","L72"],["U62","R66","U55","R34","D71","R55","D58","R83"]];
@@ -119,8 +122,10 @@ test('test [["R75","D30","R83","U83","L12","D49","R71","U7","L72"],["U62","R66",
   expect(intersectionCalc(testProgram[0], testProgram[1])).toEqual(testProgramResult);
 });
 
-// test('test [["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]]', () => {
-//   const testProgram = [["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]];
-//   const testProgramResult = 135;
-//   expect(intersectionCalc(testProgram[0], testProgram[1])).toEqual(testProgramResult);
-// });
+test('test [["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]]', () => {
+  const testProgram = [["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]];
+  const testProgramResult = 135;
+  expect(intersectionCalc(testProgram[0], testProgram[1])).toEqual(testProgramResult);
+});
+
+console.log(`Answer of day 3, part 1: ${intersectionCalc(locations[0], locations[1])}`);
